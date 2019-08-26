0c8bb5c6babba4d0bdbb2de18f6eb5875e700cda
alinaadburshina <alinaabdurshina@gmail.com>
2019-08-26 21:07:18 +0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles/style.css";
-
-//images
-import "./images/attention.png";
-import "./images/book.jpg";
-import "./images/competence.png";
-import "./images/interliving.png";
-import "./images/question.png";
-import "./images/recall.png";
